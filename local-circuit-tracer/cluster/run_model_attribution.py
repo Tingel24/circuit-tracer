3f890e4a64fe6acc201e0b4619a88e483acadabb
Luca Francis <luca@MacBook-Pro.local>
2025-07-25 11:44:48 +0200

    add logs

--- local-circuit-tracer/cluster/run_model_attribution.py
+++ local-circuit-tracer/cluster/run_model_attribution.py
@@ -17,7 +17,7 @@ out_dir.mkdir(exist_ok=True)
 
 # Load model
 model = ReplacementModel.from_pretrained(model_name, transcoder_name)
-
+print(f"Model loaded: {model_name}")
 
 if model_name.endswith("-it"):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -31,12 +31,13 @@ Only reply with one word!""" },
 else:
     # Attribution prompt
     prompt = ""  # What you want to get the graph for
+print(f"Prompt to test: {prompt}")
 max_n_logits = 10   # How many logits to attribute from, max. We attribute to min(max_n_logits, n_logits_to_reach_desired_log_prob); see below for the latter
 desired_logit_prob = 0.95  # Attribution will attribute from the minimum number of logits needed to reach this probability mass (or max_n_logits, whichever is lower)
 max_feature_nodes = 8192  # Only attribute from this number of feature nodes, max. Lower is faster, but you will lose more of the graph. None means no limit.
 batch_size=128  # Batch size when attributing
 verbose = False  # Whether to display a tqdm progress bar and timing report
-
+print("running attribution")
 # Run attribution
 attr = attribute(
     prompt=prompt,
@@ -53,9 +54,9 @@ graph_dir = Path("attribution_output/graph")
 graph_dir.mkdir(exist_ok=True, parents=True)
 
 slug = str(int(time.time()))  # this is the name that you assign to the graph
-node_threshold=0.9  # keep only the minimum # of nodes whose cumulative influence is >= 0.8
+node_threshold=0.8  # keep only the minimum # of nodes whose cumulative influence is >= 0.8
 edge_threshold=0.98  # keep only the minimum # of edges whose cumulative influence is >= 0.98
-
+print("creating graph files")
 create_graph_files(
     graph_or_path=attr,  # the graph to create files for
     slug=slug,
